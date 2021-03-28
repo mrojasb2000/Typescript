d87ace65bdddfc2bff34a43d01466a1e8b8a25f1
mavro <mrojasb2000@gmail.com>
2021-03-28 17:54:13 -0300

    RAdded validation role by user

--- decorator.ts
+++ decorator.ts
@@ -4,14 +4,27 @@ interface IDecoratorExample {
 }
 class NoRoleCheck implements IDecoratorExample {
     AnyoneCanRun(args: string): void {
+        if(!IsInRole("user")) {
+            console.log(`${currentUser.user} is not in the user role`);
+            return;
+        }
         console.log(args);
     }   
     AdminOnly(args: string): void {
+        if(!IsInRole("admin")) {
+            console.log(`${currentUser.user} is not in the admin role`);
+            return;
+        }
         console.log(args);
     }
 }
 
+function IsInRole(role: string) : boolean {
+    return currentUser.roles.some(r => r.role === role);
+}
+
 let currentUser = {user: "peter", roles : [{role:"user"}, {role:"admin"}] };
+//let currentUser = {user: "Mavro", roles: [{role:"user"}]};
 function TestDecoratorExample(decoratorMethod : IDecoratorExample) {
     console.log(`Current user ${currentUser.user}`);
     decoratorMethod.AnyoneCanRun(`Running as user`);
